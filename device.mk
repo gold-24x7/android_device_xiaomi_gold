cab7324565b0362f5ab789520f881a99b3da0749
Roshan Pius <rpius@google.com>
2023-05-14 09:13:17 +0100

    gold: Add WifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: Iab6ec7ff4a24e7e92f97231f139e31cb57e7501f
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -122,6 +122,10 @@ PRODUCT_PACKAGES += \
     init.insmod.sh \
     init.insmod.mt6833.cfg
 
+# Overlays
+PRODUCT_PACKAGES += \
+    WifiResOverlayGold
+
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
