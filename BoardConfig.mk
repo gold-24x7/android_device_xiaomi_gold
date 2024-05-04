847d1f7c2af17d510bce000763d8103c3bb2abc4
rio004 <riyon336@gmail.com>
2024-05-04 21:03:16 +0100

    gold: Inherit common MTK and Xiaomi FCMs
    Change-Id: I8fbdc3e672064202b49844cba2b915ca9bf1e597
    Signed-off-by: rio004 <riyon336@gmail.com>
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -155,6 +155,8 @@ BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 3
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
 
 # VNDK
