13722fddd2b2fd171d68b29dcfba1bd84541f45d
atharvnegi <atharvnegi2951@gmail.com>
2025-02-23 20:19:36 +0530

    gold: Add hardware/mediatek and hardware/xiaomi to Soong NameSpace
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -97,7 +97,9 @@ PRODUCT_PACKAGES += \
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/mediatek \
+    hardware/xiaomi
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/gold/gold-vendor.mk)
