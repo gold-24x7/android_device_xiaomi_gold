374622abc72fd705c775d85937e6b6f92787c89b
atharvnegi <atharvnegi2951@gmail.com>
2025-02-27 05:18:11 +0530

    gold: Import the framework manifest
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,6 +173,7 @@ BOARD_AVB_VBMETA_VENDOR_ROLLBACK_INDEX_LOCATION := 3
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     hardware/mediatek/vintf/mediatek_framework_compatibility_matrix.xml \
     hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
