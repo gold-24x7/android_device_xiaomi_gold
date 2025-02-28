10283aa1da2297a2977304c3854f7ab9b9eeda15
Sebastiano Barezzi <seba@sebaubuntu.dev>
2025-02-28 06:43:41 +0530

    gold: Build `vendor.xiaomi.hardware.fx.tunnel@1.0` from source
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>

--- device.mk
+++ device.mk
@@ -147,6 +147,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
     com.fingerprints.extension@1.0.vendor \
+    vendor.xiaomi.hardware.fx.tunnel@1.0.vendor \
     vendor.xiaomi.hardware.fingerprintextension@1.0.vendor
 
 # Gatekeeper
